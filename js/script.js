3941805398f8a2fa0efb1b9ced71c97dbbaa0fc3
Dipen prajapti <prajapati8575@gmail.com>
2021-09-26 23:49:31 -0400

    done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// WEB303 Assignment 2
+// Nikhil Patel
+
+
+
+$('#vprospect').click(function(){
+  $("#solution").load("prospect.html");
+});
+
+$( "#vconvert" ).click(function() {
+  $("#solution").load("convert.html");
+});
+
+$( "#vretain" ).click(function() {
+  $("#solution").load("retain.html");
+});
