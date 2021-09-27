a1e52753ccf5afaead21e679e7d62593de5b129d
Dipen prajapti <prajapati8575@gmail.com>
2021-09-27 00:42:21 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@
 
 $('#vprospect').click(function(){
   $("#solution").load("prospect.html");
-  $("#solution").animate({ border:" 5px ";
-  background-color: "lightblue";
-  text-align: "center";});
+  $("#solution").animate({opacity: '0.75'});
 });
 
 $( "#vconvert" ).click(function() {
   $("#solution").load("convert.html");
+  $("#solution").animate({opacity: '0.75'});
 });
 
 $( "#vretain" ).click(function() {
   $("#solution").load("retain.html");
+  $("#solution").animate({opacity: '0.75'});
 });
